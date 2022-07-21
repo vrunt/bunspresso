f9fbf4eb2d39e1a3711ecbf3a3d1755b3d4f1544
Jacob Fromm <jfromm@yext.com>
2022-07-21 13:37:33 -0400

    dynamically setting drink value state

--- islands/Calculator.tsx
+++ islands/Calculator.tsx
@@ -7,12 +7,18 @@ import { tw } from "@twind";
 
 
 export default function Calculator(props: {}) {
+
+    const [chosenDrink, setChosenDrink] = useState(
+        "aeropress"
+    )
+
     const [recipe, setRecipe] = useState({
-        coffee: props.data.aeropress.starting.coffee || 0,
-        water: props.data.aeropress.starting.water || 0,
+        coffee: 0 || props.data.find(item => item.name === `${chosenDrink}`).starting.coffee,
+        water: 0 || props.data.find(item => item.name === `${chosenDrink}`).starting.water,
         cups: 1
     })
-    console.log(props)
+
+    console.log(props.data.find(item => item.name === `${chosenDrink}`).starting.coffee)
     const btn = tw`px-2 py-1 border(gray-100 1) hover:bg-gray-200`;
     return (
         <div>
@@ -21,15 +27,15 @@ export default function Calculator(props: {}) {
                 <p class={tw`flex-grow-1 font-bold text-xl`}>water: {recipe.water * recipe.cups}</p>
             </div>
             <div class={tw`flex flex-row py-10 gap-2 w-1/2`}>
-            <p class={tw`flex-grow-1 font-bold text-xl`}>How many cups?</p>
-            <p class={tw`flex-grow-1 text-l`}>{recipe.cups}</p>
+                <p class={tw`flex-grow-1 font-bold text-xl`}>How many cups?</p>
+                <p class={tw`flex-grow-1 text-l`}>{recipe.cups}</p>
                 <button
                     class={btn}
                     onClick={() => setRecipe({
                         cups: recipe.cups - 1,
                         coffee: recipe.coffee,
                         water: recipe.water
-                        })}>
+                    })}>
                     -1
                 </button>
                 <button
@@ -38,7 +44,7 @@ export default function Calculator(props: {}) {
                         cups: recipe.cups + 1,
                         coffee: recipe.coffee,
                         water: recipe.water
-                        })}>
+                    })}>
                     +1
                 </button>
             </div>
