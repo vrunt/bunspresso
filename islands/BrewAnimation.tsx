15b2449befe9a1d52ba09beaeec9df121a5f0469
Jacob Fromm <jfromm@yext.com>
2022-11-03 15:52:39 -0400

    boolean brewing state

--- islands/BrewAnimation.tsx
+++ islands/BrewAnimation.tsx
@@ -1,12 +1,19 @@
+import { useState } from "preact/hooks";
+
 export default function BrewAnimation() {
-  return (
-    <div >
+  
+    const [isBrewing, setIsBrewing] = useState(false)
+    console.log(isBrewing)
+    return (
+    <div class={`flex flex-col items-center`}>
       <link rel="stylesheet" href="/brew.css" />
       <link rel="stylesheet" href="/lux.bootstrap.min.css" />
+      {isBrewing ? <h1>brewing</h1> : <h1>not brewing</h1>}
       <button
         type="button"
         style="height:50%; align-self:center; margin-left:1rem; text-align:center;"
         class="btn btn-outline-danger"
+        onClick={() => setIsBrewing(!isBrewing)}
       >
         BREW
       </button>
--- routes/brew.tsx
+++ routes/brew.tsx
@@ -1,11 +1,12 @@
 import BrewAnimation from "../islands/BrewAnimation.tsx";
 
+
 export default function Brew() {
+    
   return (
     <div class={`min-h-screen flex flex-row items-center`}>
       <link rel="stylesheet" href="/brew.css" />
       <div class={`flex flex-col items-center`}>
-      <h1>brew page</h1>
         <BrewAnimation />
       </div>
     </div>
